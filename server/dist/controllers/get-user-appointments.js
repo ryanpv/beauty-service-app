2564a4e4f871c4a0506ff374a4383916a4aea86b
Ryan <ryanpvoong@gmail.com>
2023-12-23 14:48:30 -0500

    added more join statements to join multiple tables to get actual values instead of key references

--- server/dist/controllers/get-user-appointments.js
+++ server/dist/controllers/get-user-appointments.js
@@ -2,10 +2,15 @@ import { pool } from "../queries.js";
 export const getUserAppointments = (req, res) => {
     const { userId } = req.params;
     pool.query(`
-    SELECT appointments.*, status_types.status 
+    SELECT appointments.*, status_types.status, service_types.service_name
     FROM appointments
     JOIN status_types
       ON appointments.status = status_types.id
+    JOIN appointment_line_items
+      ON appointments.id = appointment_line_items.appointments_id
+    JOIN service_types
+      ON appointment_line_items.service_types_id = service_types.id
+
     WHERE users_id = $1
   `, [userId], (error, results) => {
         if (error) {
--- server/src/controllers/get-user-appointments.ts
+++ server/src/controllers/get-user-appointments.ts
@@ -5,10 +5,15 @@ export const getUserAppointments = (req: Request, res: Response) => {
   const { userId } = req.params;
 
   pool.query(`
-    SELECT appointments.*, status_types.status 
+    SELECT appointments.*, status_types.status, service_types.service_name
     FROM appointments
     JOIN status_types
       ON appointments.status = status_types.id
+    JOIN appointment_line_items
+      ON appointments.id = appointment_line_items.appointments_id
+    JOIN service_types
+      ON appointment_line_items.service_types_id = service_types.id
+
     WHERE users_id = $1
   `, [userId], (error, results) => {
     if (error) {
