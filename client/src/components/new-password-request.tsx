6eaf37adb04bf5e157773c633d1b502f93d88b06
Ryan <ryanpvoong@gmail.com>
2023-12-30 16:14:12 -0500

    added some form logic to connect to server

--- client/src/components/new-password-request.tsx
+++ client/src/components/new-password-request.tsx
@@ -1,25 +1,50 @@
 import React from 'react'
 
-export default function NewPasswordRequest() {
-  // 
+const NewPasswordRequest:React.FC = () => {
+  const emailInputRef = React.useRef<HTMLInputElement | null>(null);
+
+  const submitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const emailInputValue = emailInputRef.current!.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+console.log("email input: ", emailInputValue);
+
+    if (emailRegex.test(emailInputValue)) {
+      fetch("https://localhost:3001/password-resets",{
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ email: emailInputValue })
+      });
+      console.log("is email")
+      
+    } else {
+      console.log("NOT email")
+    }
+  }
+
   return (
     <div>
       <h2>Request Password Reset</h2>
       <p>Forgot your password? Enter your email address to request a new password.</p>
 
-      
       <div>
-        <form>
+        <form onSubmit={ submitForm }>
           <div>
             <label>
               Email Address
             </label>
-            <input placeholder='' />
+            <input type='email' ref={ emailInputRef } />
           </div>
 
-          <button>Submit Request</button>
+          <button type='submit'>Submit Request</button>
         </form>
       </div>
     </div>
   )
-}
+};
+
+export default NewPasswordRequest;
--- client/src/components/reset-password-form.tsx
+++ client/src/components/reset-password-form.tsx
@@ -1,28 +1,67 @@
 import React from 'react'
 
-export default function ResetPassword() {
+const ResetPassword:React.FC = () => {
+  const newPassRef = React.useRef<HTMLInputElement | null>(null);
+  const confirmNewPassRef = React.useRef<HTMLInputElement | null>(null);
+  const queryParams = new URLSearchParams(window.location.search);
+  const token = queryParams.get('recovery-token');
+  
+  const seeValue = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newPassVal = newPassRef.current?.value;
+    console.log(newPassVal);
+  };
+
+  const submitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const newPassValue = newPassRef.current?.value;
+      const confirmPassValue = confirmNewPassRef.current?.value;
+  
+      if (newPassValue !== undefined && newPassValue === confirmPassValue) {
+        fetch(`https://localhost:3001/password-resets/${ token }`,{
+          method: "PUT",
+          credentials: "include",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify({ newPassword: newPassValue })
+        });
+      } else {
+        console.log("Passwords do not match")
+      }
+  
+      newPassRef.current!.value = '';
+      confirmNewPassRef.current!.value = '';
+    } catch (error) {
+      console.log("Password reset error: ", error)
+    }
+  };
+
   return (
     <div>
       <h1>Reset your password</h1>
 
       <div>
-        <form>
+        <form onSubmit={submitForm}>
           <div>
             <label>
               New Password
             </label>
-            <input />
+            <input type='text' onChange={(e) => seeValue(e)} ref={ newPassRef }/>
           </div>
 
           <div>
             <label>
               Confirm New Password
             </label>
-            <input />
+            <input type='text' ref={ confirmNewPassRef }/>
           </div>
           <button type='submit'>Change Password</button>
         </form>
       </div>
     </div>
   )
-}
+};
+
+export default ResetPassword;
