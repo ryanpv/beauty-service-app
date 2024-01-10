80e8543c80442f0bf63a5a872496175eb7ad9bab
Ryan <ryanpvoong@gmail.com>
2024-01-09 22:27:21 -0500

    added fetch functionality to single appointm ent update. also updated serverside controller to GET more specific data from appointment

--- client/src/components/update-appointment.tsx
+++ client/src/components/update-appointment.tsx
@@ -1,26 +1,71 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DatePicker from 'react-datepicker';
+import { useStateContext } from '../contexts/state-contexts';
 import { useParams } from 'react-router-dom';
 // import 'react-datepicker/dist/react-datepicker.module.css'
 
 const UpdateAppointment: React.FC = () => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1; // 0-index, must +1 to get accurate month value
-  const day = date.getDate();
-  const currentDate = `${ year }-${ month }-${ day }`
+  type Appointment = {
+    id: number;
+    date: string;
+    email: string;
+    name?: string;
+    service_name: string;
+    status: string;
+    time: string;
+    users_id: number;
+    price_paid: string;
+  };
 
-  const userId = 1
+  const appointmentState = {
+    id: 0,
+    date: "",
+    email: "",
+    name: "Test Client",
+    service_name: "",
+    status: "",
+    time: "",
+    users_id: 0,
+    price_paid: ""
+  };
 
+  const { currentUser } = useStateContext();
+  const [appointment, setAppointment] = useState<Appointment>(appointmentState)
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const { appointmentId } = useParams();
 
-  const appointment = async() => {
-    const getAppointment = await fetch(`https://localhost:3001/`)
-  }
+  const date = new Date(appointment.date);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 0-indexed, must +1 to get accurate month value
+  const day = date.getDate();
+  const appointmentDate = `${ year }-${ month }-${ day }`
 
-  console.log("appt id: ", appointmentId)
-  console.log("Date: ", new Date("2023-05-12"))
+  console.log(appointmentDate)
+  
+  useEffect(() => {
+    fetchAppointment();
+  }, []);
+
+  const fetchAppointment = async() => {
+    try {
+      const getAppointment = await fetch(`https://localhost:3001/users/${ currentUser }/appointments/${ appointmentId }`, {
+        method: "GET",
+        credentials: "include"
+      });
+  
+      const result = await getAppointment.json();
+      if (result.length > 0) {
+        setAppointment(result[0]);
+      } else {
+        throw new Error("Appointment not found");
+      }
+    } catch (error) {
+      console.log("Error fetching users appointment", error);
+    }
+  };
+
+  console.log("appt id: ", appointment)
+
   return (
     <div className='container flex flex-col space-y-6 p-5 max-w-screen-lg'>
       <h1 className='text-center'>Update Appointment</h1>
@@ -31,6 +76,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Client</label>
             <div>
               <input
+                value={ appointment.name }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
             </div>
@@ -39,6 +85,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Contact Info</label>
             <div>
               <input
+                value={ appointment.email }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
             </div>
@@ -47,6 +94,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Service</label>
             <div>
               <input
+                value={ appointment.service_name }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
             </div>
@@ -55,6 +103,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Status</label>
             <div>
               <input
+                value={ appointment.status }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
             </div>
@@ -63,6 +112,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Date</label>
             <div>
               <input
+                value={ appointmentDate }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
               <DatePicker 
@@ -76,6 +126,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Time</label>
             <div>
               <input
+                value={ appointment.time }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
             </div>
@@ -84,6 +135,7 @@ const UpdateAppointment: React.FC = () => {
             <label>Price</label>
             <div>
               <input
+                value={ appointment.price_paid }
                 className='block w-full py-1.5 px-2.5 border-0 rounded-sm ring-1 ring-pink-300'
               />
             </div>
