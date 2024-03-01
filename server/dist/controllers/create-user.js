7d69ea89508064df800cd2d1031532cb9cbfc99c
Ryan <ryanpvoong@gmail.com>
2024-03-01 14:28:23 -0500

    removed secure config for cookie

--- server/dist/controllers/create-user.js
+++ server/dist/controllers/create-user.js
@@ -46,8 +46,8 @@ export const createUser = async (req, res) => {
                 req.session.isAuthenticated = true;
                 req.session.userRole = "client";
                 req.session.accessToken = jwtToken;
-                res.cookie("user", jwtToken, { httpOnly: false, secure: true, domain: 'https://beauty-service-app-1.onrender.com' });
-                res.cookie('id', req.sessionID, { httpOnly: true, secure: true, domain: 'https://beauty-service-app-1.onrender.com' });
+                res.cookie("user", jwtToken, { httpOnly: false, domain: 'https://beauty-service-app-1.onrender.com' });
+                res.cookie('id', req.sessionID, { httpOnly: true, domain: 'https://beauty-service-app-1.onrender.com' });
                 const emailMsg = {
                     from: process.env.GMAIL_ACCOUNT,
                     to: userEmail,
