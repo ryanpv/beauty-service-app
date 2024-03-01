ef2943567a63eda8be033d8b25fc836df64f60b8
Ryan <ryanpvoong@gmail.com>
2024-03-01 09:31:35 -0500

    fixed database name

--- server/dist/queries.js
+++ server/dist/queries.js
@@ -3,7 +3,7 @@ const Pool = pg.Pool;
 export const pool = new Pool({
     user: process.env.NODE_ENV === 'development' ? process.env.PSQL_USER : process.env.PSQL_PRODUSER,
     host: process.env.NODE_ENV === 'development' ? process.env.PSQL_LOCALHOST : process.env.PSQL_PRODHOST,
-    database: 'beauty_service_app',
+    database: process.env.NODE_ENV === 'development' ? 'beauty_service_app' : 'beauty_service_app_db',
     password: process.env.NODE_ENV === 'development' ? process.env.PSQL_PASSWORD : process.env.PSQL_PRODPASS,
     port: parseInt(process.env.PSQL_PORT),
     ssl: true
