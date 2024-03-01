cfc767384efb09bf1a2c8a462d0c696c575708fa
Ryan <ryanpvoong@gmail.com>
2024-02-29 21:32:46 -0500

    updated db host

--- server/dist/queries.js
+++ server/dist/queries.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 const Pool = pg.Pool;
 export const pool = new Pool({
     user: process.env.PSQL_USER,
-    host: 'localhost',
+    host: process.env.NODE_ENV === 'development' ? process.env.PSQL_LOCALHOST : process.env.PSQL_PRODHOST,
     database: 'beauty_service_app',
     password: process.env.PSQL_PASSWORD,
     port: parseInt(process.env.PSQL_PORT),
--- server/src/queries.ts
+++ server/src/queries.ts
@@ -4,7 +4,7 @@ const Pool = pg.Pool;
 
 export const pool = new Pool({
   user: process.env.PSQL_USER,
-  host: 'localhost',
+  host: process.env.NODE_ENV === 'development' ? process.env.PSQL_LOCALHOST : process.env.PSQL_PRODHOST,
   database: 'beauty_service_app',
   password: process.env.PSQL_PASSWORD,
   port: parseInt(process.env.PSQL_PORT),
