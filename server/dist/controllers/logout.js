c2b0a318d44c8954d2ea493d649ac1fa2562295e
Ryan <ryanpvoong@gmail.com>
2024-03-01 18:47:18 -0500

    added domain option to clearcookie function

--- server/dist/controllers/logout.js
+++ server/dist/controllers/logout.js
@@ -1,11 +1,12 @@
 export const logout = (req, res) => {
+    const domain = process.env.NODE_ENV === 'production' ? '.polishbycin.com' : 'localhost';
     req.session.destroy((error) => {
         if (error) {
             res.status(400).json({ message: "Failed to log out" });
         }
         else {
-            res.clearCookie('id');
-            res.clearCookie('user');
+            res.clearCookie('id', { domain: domain });
+            res.clearCookie('user', { domain: domain });
             res.status(200).json({ message: "Logout successful" });
         }
     });
--- server/src/controllers/logout.ts
+++ server/src/controllers/logout.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 
 export const logout = (req: Request, res: Response) => {
+  const domain = process.env.NODE_ENV === 'production' ? '.polishbycin.com' : 'localhost'
+
   req.session.destroy((error) => {
     if (error) {    
       res.status(400).json({ message: "Failed to log out" });
     } else {      
-      res.clearCookie('id');
-      res.clearCookie('user');
+      res.clearCookie('id', { domain: domain });
+      res.clearCookie('user', { domain: domain });
 
       res.status(200).json({ message: "Logout successful" });
     }
