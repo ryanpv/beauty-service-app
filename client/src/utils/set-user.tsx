ea766d731fbd18a08a256436ce5ff7a1c039c123
Ryan <ryanpvoong@gmail.com>
2024-04-15 12:30:23 -0400

    log removed

--- client/src/utils/set-user.tsx
+++ client/src/utils/set-user.tsx
@@ -3,7 +3,6 @@ import Cookies from 'js-cookie';
 
 export const setUser = () => {
   const userLogged = Cookies.get('user');  
-  console.log('set user: ', userLogged);
   
   const initialUserState = {
     id: 0,
