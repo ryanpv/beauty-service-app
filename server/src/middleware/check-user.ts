97e98acf23a2ea77c32d2b675ae6c14f6e6c04b6
Ryan <ryanpvoong@gmail.com>
2024-04-14 22:39:46 -0400

    another attempt to remove connectsid

--- server/src/middleware/check-user.ts
+++ server/src/middleware/check-user.ts
@@ -44,17 +44,18 @@ export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
 
       res.cookie('user', null, { httpOnly: false });
       res.cookie('id', null, { httpOnly: false });
-      res.clearCookie('connect.sid', { domain: domain });
+      // res.clearCookie('connect.sid', { domain: domain });
       res.status(401).json({ message: "Invalid token" });
     }
   } catch (error) {
     console.error("check-user.verifyUser error: ", error)
-    res.clearCookie('connect.sid', { domain: domain });
     res.cookie('user', null, { httpOnly: false });
     res.cookie('id', null, { httpOnly: false });
     req.session.destroy((error) => {
       console.error("error with check-user.verifyUser", error)
     });
+    res.cookie('connect.sid', null)
+    // res.clearCookie('connect.sid', { domain: domain });
 
     res.status(401).json({
       message: "Unsuccessful authentication."
