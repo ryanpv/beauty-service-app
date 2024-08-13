775e6989f065ba6c3bfb63c018d63a3b72253906
Ryan <ryanpvoong@gmail.com>
2024-08-13 13:38:31 -0400

    fixed error handling for services route

--- server/src/controllers/get-all-services.ts
+++ server/src/controllers/get-all-services.ts
@@ -1,18 +1,17 @@
 import { Response, Request } from 'express';
 import { pool } from '../queries.js';
 
-export const getAllServices = (req: Request, res: Response) => {
-
-  pool.query(`
-    SELECT service_types.*, service_categories.service_category_name FROM service_types
-      JOIN service_categories
-        ON service_types.service_categories_id = service_categories.id
-      ORDER BY service_types.id`, (error, results) => {
-          if (error) {
-            console.log(`GET ALL SERVICES ERROR: ${ error }`);
-            res.status(400).json({ message: "Unable to fetch list of services." })
-          }
-
-          res.status(200).json(results.rows);
-        });
+export const getAllServices = async (req: Request, res: Response) => {
+  try {
+    const services = await pool.query(`
+      SELECT service_types.*, service_categories.service_category_name FROM service_types
+        JOIN service_categories
+          ON service_types.service_categories_id = service_categories.id
+        ORDER BY service_types.id`)
+      
+    res.status(200).json(services.rows)
+  } catch (error) {
+    console.log(`GET ALL SERVICES ERROR: ${ error }`);
+    res.status(400).json({ message: "Unable to fetch list of services." });
+  }
 };
